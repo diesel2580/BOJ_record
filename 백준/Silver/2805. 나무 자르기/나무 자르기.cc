dd13287dbbcd31e90c4cf71c466ad0c4c2f6ce2e
diesel2580 <107855914+diesel2580@users.noreply.github.com>
2023-04-12 15:13:20 +0900

    [Silver II] Title: 나무 자르기, Time: 672 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -6,34 +6,29 @@ long long int n, m;
 int num[1000009];
 int flag = 0;
 
-void search(int start, int end) {
+int search(int start, int end) {
 
 	long long int wood = 0;
 	int mid = (start + end) / 2;
-	long long int tmpW = 2000000000, tmpH = mid;;
+	long long int tmpW = 2000000000, tmpH = mid;
 
 	if (start > end) {
-		cout << tmpH;
-		return;
+		return tmpH;
 	}
 
 	for (int i = 0; i < n; i++) {
 		if (num[i] - mid > 0)
 			wood += num[i] - mid;
 	}
-	if (wood == m) {
-		cout << mid;
-		return;
-	}
-	else if (wood > m) {
-		search(mid + 1, end);
+	if (wood >= m) {
 		if (tmpW < wood) {
 			tmpW = wood;
 			tmpH = mid;
 		}
+		return search(mid + 1, end);
 	}
-	else {
-		search(start, mid - 1);
+	else if (wood < m) {
+		return search(start, mid - 1);
 	}
 }
 
@@ -44,8 +39,8 @@ int main() {
 		cin >> num[i];
 	}
 	sort(num, num + n);
-	
-	search(0, num[n - 1]);
+
+	cout<< search(0, num[n - 1]);
 
 
 	return 0;

41e63e7a82b7366589edc1210829627e8e2f9de1
diesel2580 <107855914+diesel2580@users.noreply.github.com>
2023-07-06 01:16:59 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	string s;
+	cin >> n >> s;
+	int cnt = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') cnt++;
+	}
+	cout << cnt;
+
+	return 0;
+}

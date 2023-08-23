72f9a55f6405e98f5e3caf7712b97633e805a13d
diesel2580 <107855914+diesel2580@users.noreply.github.com>
2023-08-23 18:53:43 +0900

    [Silver II] Title: 에디터, Time: 96 ms, Memory: 19860 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,59 +1,104 @@
-#include <iostream>
-#include <stack>
-using namespace std;
+#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
 
-string str, str2;
-stack<char> s;
-stack<char> s2;
+typedef struct Dlist {
+	struct Dlist* prev;
+	struct Dlist* next;
+	char value;
+};
+
+Dlist* head, * tail, * cur;
+int size = 0;
+
+void init() {
+	head = (Dlist*)malloc(sizeof(Dlist));
+	tail = (Dlist*)malloc(sizeof(Dlist));
+	head->value = '\0';
+	head->prev = NULL;
+	head->next = tail;
+	tail->value = '\0';
+	tail->prev = head;
+	tail->next = NULL;
+	//커서: 현재 위치
+	cur = (Dlist*)malloc(sizeof(Dlist));
+	cur = tail->prev;
+}
+
+void insert(Dlist* head, char data) {
+	Dlist* newNode = (Dlist*)malloc(sizeof(Dlist));
+	newNode->next = head->next;
+	newNode->prev = head;
+	newNode->value = data;
+	newNode->next->prev = newNode;
+	head->next = newNode;
+	size++;
+	cur = newNode;
+}
+// a
+
+void moveleft(Dlist* node) {
+	//if (cur->prev != head) cur = cur->prev;
+	if (node != head) cur = node->prev;
+}
+
+void moveright(Dlist* node) {
+	if (node->next != tail) cur = node->next;
+}
+
+void eraseleft(Dlist* node) {
+	if (node != head) {
+		cur = node->prev;
+		node->next->prev = node->prev;
+		node->prev->next = node->next;
+		//cur = node->prev;
+	}
+}
+//a b(node) c
+
+void print() {
+	Dlist* tmp = (Dlist*)malloc(sizeof(Dlist));
+	tmp = head->next;
+	while (tmp != tail) {
+		printf("%c", tmp->value);
+		tmp = tmp->next;
+	}
+}
 
 int main() {
 
-	int n;
-	cin >> str;
-	cin >> n;
-	for (int i = 0; i < str.size(); i++) {
-		s.push(str[i]);
-	}
-	for (int i = 0; i < n; i++) {
-		char cmd;
-		char tmp;
-		cin >> cmd;
-		switch (cmd) {
-		case 'P': 
-			//char tmp;
-			cin >> tmp;
-			s.push(tmp);
-			break;
-		case 'L': 
-			if (s.empty()) break;
-			tmp = s.top();
-			s.pop();
-			s2.push(tmp);
-			break;
-		case 'D': 
-			if (s2.empty()) break;
-			tmp = s2.top();
-			s2.pop();
-			s.push(tmp);
-			break;
-		case 'B':
-			if (s.empty()) break;
-			s.pop();
-			break;
-		}
-	}
-	while (!s2.empty()) {
-		s.push(s2.top());
-		s2.pop();
+	char str[100009];
+	scanf("%s", str);
+	init();
+	for (int i = 0; str[i] != '\0'; i++) {
+		insert(cur, str[i]);
 	}
 
-	while (!s.empty()) {
-		str2 += s.top();
-		s.pop();
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		char ord, tmp;
+		scanf(" %c", &ord);
+		switch (ord) {
+		case'P': 
+			scanf(" %c", &tmp);
+			insert(cur, tmp);
+			break;
+		case 'L': 
+			moveleft(cur);
+			break;
+		case 'D': 
+			moveright(cur);
+			break;
+		case 'B': 
+			eraseleft(cur);
+			break;
+		}
+		
 	}
-	for (int i = str2.size() - 1; i >= 0; i--) {
-		cout << str2[i];
-	}
+	print();
 
 	return 0;
 }
+
+
